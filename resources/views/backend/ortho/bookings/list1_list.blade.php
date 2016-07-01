3058936b53557d48dc193b73685ca36ab86c2c8f
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2016-07-01 08:15:24 +0700

    commit list1_list

---
+++ resources/views/backend/ortho/bookings/list1_list.blade.php
@@ -0,0 +1,127 @@
+@extends('backend.ortho.ortho')
+
+@section('content')
+	<!-- content list1 list -->
+    <section id="page">
+      <div class="container content-page">
+        <h3>各種リスト表示　＞　「TEL待ちリスト」の表示</h3>
+        <div class="table-responsive">
+          <table class="table table-bordered table-striped">
+            <tbody>
+              <tr>
+                <td class="col-title" align="center">医院名</td>
+                <td class="col-title" align="center">最終来院日</td>
+                <td class="col-title" align="center">カルテNo</td>
+                <td class="col-title" align="center">患者名</td>
+                <td class="col-title" align="center">電話番号</td>
+                <td class="col-title" align="center">業務内容-1-2</td>
+                <td class="col-title" align="center">備考</td>
+                <td class="col-title" align="center">予約情報の編集</td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+              <tr>
+                <td>たい矯正歯科</td>
+                <td>2016/05/01</td>
+                <td>123456</td>
+                <td>杉元　俊彦</td>
+                <td>086-000-0000</td>
+                <td>SET　SET</td>
+                <td></td>
+                <td align="center"><input onclick="location.href='booking_edit.html'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>    
+    </section>
+  <!-- End content list1 list -->
+@endsection

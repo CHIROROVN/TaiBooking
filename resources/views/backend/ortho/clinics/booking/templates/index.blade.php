264419d6977bcffa4189e7b5f2ce30723899908e
dophu17 <dophu17@gmail.com>
2016-07-05 13:00:04 +0700

    tets

--- resources/views/backend/ortho/clinics/booking/templates/index.blade.php
+++ resources/views/backend/ortho/clinics/booking/templates/index.blade.php
@@ -5,11 +5,31 @@
     <section id="page">
       <div class="container content-page">
         <h3>医院情報管理　＞　{{ $clinic->clinic_name }}　＞　予約雛形の一覧</h3>
+
+       <div class="msg-alert-action margin-top-15">
+          @if ($message = Session::get('success'))
+            <div class="alert alert-success  alert-dismissible fade in" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <ul class="no-margin-bottom"><strong><li> {{ $message }}</li></strong></ul>
+            </div>
+          @elseif($message = Session::get('danger'))
+            <div class="alert alert-danger alert-dismissible fade in" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <ul class="no-margin-bottom"><strong><li> {{ $message }}</li></strong></ul>
+            </div>
+          @endif
+        </div>
+
         <div class="row">
           <div class="col-md-12 text-right">
             <input onclick="location.href='{{ route('ortho.clinics.booking.templates.regist', [ $clinic->clinic_id ]) }}'" value="雛形の新規作成" type="button" class="btn btn-sm btn-page">
           </div>
         </div>
+
         <table class="table table-bordered table-striped treatment2-list">
           <tbody>
             <tr>
@@ -28,18 +48,18 @@
               <?php $i++; ?>
                 <tr>
                   <td>{{$mbt->mbt_name}}</td>
-                  <td align="center"><a href="{{route('ortho.clinics.booking.templates.edit', [$clinic_id, $mbt->mbt_id])}}" class="btn btn-sm btn-edit">編集</a></td>
+                  <td align="center"><a href="{{route('ortho.clinics.booking.templates.edit', [$clinic->clinic_id, $mbt->mbt_id])}}" class="btn btn-sm btn-edit">編集</a></td>
                   <td align="center">
-                      <button onclick="location.href='{{ url('ortho/clinics/'.$clinic_id.'/booking/templates/orderby-top?id='.$mbt->mbt_id) }}'" class="@if($i < 2) {{'hidden'}} @endif">TOP
+                      <button onclick="location.href='{{ url('ortho/clinics/'.$clinic->clinic_id.'/booking/templates/orderby-top?id='.$mbt->mbt_id) }}'" class="@if($i < 2) {{'hidden'}} @endif">TOP
                     </td>
                     <td align="center" class="">
-                      <button onclick="location.href='{{ url('ortho/clinics/'.$clinic_id.'/booking/templates/orderby-up?id='.$mbt->mbt_id) }}'" class="@if($i < 2) {{'hidden'}} @endif">↑</button>
+                      <button onclick="location.href='{{ url('ortho/clinics/'.$clinic->clinic_id.'/booking/templates/orderby-up?id='.$mbt->mbt_id) }}'" class="@if($i < 2) {{'hidden'}} @endif">↑</button>
                     </td>
                     <td align="center">
-                      <button onclick="location.href='{{ url('ortho/clinics/'.$clinic_id.'/booking/templates/orderby-down?id='.$mbt->mbt_id) }}'" class="@if($i == $max) {{'hidden'}} @endif">↓</button>
+                      <button onclick="location.href='{{ url('ortho/clinics/'.$clinic->clinic_id.'/booking/templates/orderby-down?id='.$mbt->mbt_id) }}'" class="@if($i == $max) {{'hidden'}} @endif">↓</button>
                     </td>
                     <td align="center" class="">
-                      <button onclick="location.href='{{ url('ortho/clinics/'.$clinic_id.'/booking/templates/orderby-last?id='.$mbt->mbt_id) }}'" class="@if($i == $max) {{'hidden'}} @endif">LAST</button>
+                      <button onclick="location.href='{{ url('ortho/clinics/'.$clinic->clinic_id.'/booking/templates/orderby-last?id='.$mbt->mbt_id) }}'" class="@if($i == $max) {{'hidden'}} @endif">LAST</button>
                     </td>
                 </tr>
               @endforeach
@@ -62,6 +82,7 @@
             </tr> -->
           </tbody>
         </table>
+
         <div class="row margin-bottom" style="display: block; float: right;">
           <div class="col-md-12 text-center">
             <input onclick="location.href='{{route('ortho.clinics.index')}}'" value="医院一覧に戻る" type="button" class="btn btn-sm btn-page">
--- resources/views/backend/ortho/clinics/booking/templates/regist.blade.php
+++ resources/views/backend/ortho/clinics/booking/templates/regist.blade.php
@@ -2,59 +2,35 @@
 
 @section('content')
 	<!-- Content clinic booking template regist -->
+{!! Form::open(array('route' => ['ortho.clinics.booking.templates.regist', $clinic->clinic_id], 'method' => 'post', 'enctype'=>'multipart/form-data')) !!}
   <section id="page">
     <div class="container">
       <div class="row content content--page">
-        <h3>医院情報管理　＞　たい矯正歯科　＞　予約雛形の新規登録</h3>
+        <h3>医院情報管理　＞　{{ $clinic->clinic_name }}　＞　予約雛形の新規登録</h3>
         <table class="table table-bordered">
           <tr>
-            <td class="col-title"><label for="txtTemName">雛形名</label></td>
+            <td class="col-title"><label for="mbt_name">雛形名</label></td>
             <td>
-              <input name="txtTemName" id="txtTemName" value="" type="text" class="form-control form-control--default">
+              <input name="mbt_name" id="mbt_name" value="{{ old('mbt_name') }}" type="text" class="form-control form-control--default">
+              <span class="error-input">@if ($errors->first('mbt_name')) {!! $errors->first('mbt_name') !!} @endif</span>
               <!-- <input type="button" class="btn btn-sm btn-page no-border" name="button" value="保存する"> -->
             </td>
           </tr>
         </table>
-        <div class="table-responsive">
-          <table class="table table-bordered table-shift-set">
-            <tr>
-              <td align="center">時間</td>
-              @foreach ( $facilitys as $facility )
-              <td align="center">{{ $facility->facility_name }}</td>
-              @endforeach
-            </tr>
-
-            <!-- check "brown", "green", "blue" color -->
-            @foreach ( $times as $time )
-            <?php
-              $tmp_arr = explode(':', $time);
-              $hour = $tmp_arr[0]; // printf( "%02d", $tmp_arr[0] );
-              $minute = $tmp_arr[1]; //printf( "%02d", $tmp_arr[1] );
-            ?>
-            <tr>
-              <td align="center">{{ $time }}～</td>
-              @foreach ( $facilitys as $facility )
-                
-                <td align="center" class="col-brown"><img src="{{ asset('') }}public/backend/ortho/common/image/img-col-shift-set.png" /></td>
-                
-              @endforeach
-            </tr>
-            @endforeach
-            
-          </table>
+        
+      </div>
+      <div class="row margin-bottom">
+        <div class="col-md-12 text-center">
+          <input name="" id="button2" value="保存する" type="submit" class="btn btn-sm btn-page mar-right">
         </div>
       </div>
       <div class="row margin-bottom">
         <div class="col-md-12 text-center">
-          <input name="button2" id="button2" value="保存する" type="submit" class="btn btn-sm btn-page mar-right">
-        </div>
-      </div>
-      <div class="row margin-bottom">
-        <div class="col-md-12 text-center">
-          <input onclick="location.href='clinic_list.html'" value="医院一覧に戻る" type="button" class="btn btn-sm btn-page">
+          <input onclick="location.href='{{ route('ortho.clinics.index') }}'" value="医院一覧に戻る" type="button" class="btn btn-sm btn-page">
         </div>
       </div>
     </div>
   </section>
+</form>
   <!-- End content clinic booking template regist -->
 @endsection

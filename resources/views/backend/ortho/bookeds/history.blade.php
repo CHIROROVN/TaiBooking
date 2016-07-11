47fd5b49c525abeab3f9bfdf670ff9cd0d23f058
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2016-07-11 22:22:22 +0700

    update

---
+++ resources/views/backend/ortho/bookeds/history.blade.php
@@ -0,0 +1,80 @@
+@extends('backend.ortho.ortho')
+
+@section('content')
+<section id="page">
+  <div class="container content-page">
+    <h3 class="margin-bottom">予約管理　＞　予約の一覧</h3>
+
+    <div>
+      <div class="form-inline">
+          {!! Form::open(array('route' => 'ortho.bookeds.history', 'method' => 'get', 'enctype'=>'multipart/form-data')) !!}
+          <select title="医院" name="s_clinic_id" class="form-control">
+            <option value="">医院</option>
+            @foreach ( $clinics as $clinic)
+            <option value="{{ $clinic->clinic_id }}" @if($s_clinic_id == $clinic->clinic_id) selected="" @endif>{{ $clinic->clinic_name }}</option>
+            @endforeach
+          </select>
+          　
+          <select name="s_booking_date" id="s_booking_date" class="form-control" style="margin-left: -19px;">
+            <option value="">----日</option>
+            @foreach ( $dates as $date )
+            <option value="{{ $date }}" @if($s_booking_date == $date) selected="" @endif>{{ formatDate($date, '/') }}({{ DayJp($date) }}日)</option>
+            @endforeach
+          </select>
+
+          <input name="" value="検索" type="submit" class="btn btn-sm btn-page">
+          </form>
+      </div>
+    </div>
+
+    <table class="table table-bordered table-striped treatment2-list">
+      <tbody>
+        <tr>
+          <td  class="col-title" align="center">時間帯</td>
+          <td  class="col-title"align="center">患者名</td>
+          <td class="col-title" align="center">本日の内容</td>
+          <td class="col-title" align="center">編集</td>
+          <td class="col-title" align="center">次回予約</td>
+        </tr>
+        @if ( !count($bookeds) )
+        <tr><td colspan="5" align="center">{{ trans('common.no_data_correspond') }}</td></tr>
+        @else
+          @foreach ( $bookeds as $booked )
+          <tr>
+            <td>{{ splitHourMin($booked->booking_start_time) }}～{{ toTime($booked->booking_start_time, $booked->booking_total_time) }}</td>
+            <td>{{ $booked->p_no }}　{{ $booked->p_name }}（{{ $booked->p_name_kana }}）</td>
+            <td>
+              @if ( $booked->service_1_kind == 1 )
+              {{ @$services[$booked->service_1] }}
+              @elseif ( $booked->service_1_kind == 2 )
+              {{ @$treatment1s[$booked->service_1] }}
+              @endif
+              ,
+              @if ( $booked->service_2_kind == 1 )
+              {{ @$services[$booked->service_2] }}
+              @elseif ( $booked->service_2_kind == 2 )
+              {{ @$treatment1s[$booked->service_2] }}
+              @endif
+            </td>
+            <td align="center"><input onclick="location.href='{{ route('ortho.bookeds.history.edit', [ $booked->booking_id ]) }}'" value="編集" type="button" class="btn btn-xs btn-page"></td>
+            <td align="center">
+              @if ( $booked->booking_date > $currentDay )
+              {{ formatDate($booked->booking_date) }} {{ splitHourMin($booked->booking_start_time) }}～{{ toTime($booked->booking_start_time, $booked->booking_total_time) }}
+              @else
+              <input onclick="location.href='{{ route('ortho.bookings.booking_search') }}'" value="次回予約" type="button" class="btn btn-xs btn-page">
+              @endif
+            </td>
+          </tr>
+          @endforeach
+        @endif
+      </tbody>
+    </table>
+
+    <div class="row margin-bottom">
+      <div class="col-md-12 text-center">
+        {!! $bookeds->render(new App\Pagination\SimplePagination($bookeds))  !!}
+      </div>
+    </div>
+  </div>    
+</section>
+@endsection
---
+++ resources/views/backend/ortho/bookings/list3_list.blade.php
@@ -0,0 +1,80 @@
+@extends('backend.ortho.ortho')
+
+@section('content')
+	<!-- content list1 list -->
+  <section id="page">
+    <div class="container content-page">
+      <h3>各種リスト表示　＞　「リコールリスト」の表示</h3>
+      <table class="table table-bordered">
+        <tr>
+          <td class="col-title"><label for="textName">当初予約日</label></td>
+          <td>
+            {!! Form::open(array('route' => 'ortho.bookings.list3_list', 'method' => 'get', 'enctype'=>'multipart/form-data')) !!}
+            <select name="booking_recall_yy" id="booking_recall_yy" class="form-control form-control--small mar-right">
+              <option value="">----年</option>
+              @foreach ( $years as $year )
+              <option value="{{ $year }}" @if($booking_recall_yy == $year) selected="" @endif>{{ $year }}年</option>
+              @endforeach
+            </select>
+         
+            <select name="booking_recall_mm" id="booking_recall_mm" class="form-control form-control--small mar-right">
+              <option value="">--月</option>
+              @for ( $i = 1; $i <= 12; $i++ )
+              <?php $i = ($i < 10) ? ('0' . $i) : $i; ?>
+              <option value="{{ $i }}" @if($booking_recall_mm == $i) selected="" @endif>{{ $i }}月</option>
+              @endfor
+            </select>
+            <input name="" id="button" value="表示" type="submit" class="btn btn-sm btn-page">
+            </form>
+          </td>
+        </tr>
+      </table>
+      <div class="table-responsive">
+        @if ( !empty($list3s) && count($list3s) > 0 )
+        <table class="table table-bordered table-striped">
+          <tbody>
+            <tr>
+              <td class="col-title" align="center">医院名</td>
+              <td class="col-title" align="center">最終来院日</td>
+              <td class="col-title" align="center">当初予約日時</td>
+              <td class="col-title" align="center">カルテNo</td>
+              <td class="col-title" align="center">患者名</td>
+              <td class="col-title" align="center">電話番号</td>
+              <td class="col-title" align="center">最終処置内容-1-2</td>
+              <td class="col-title" align="center">備考</td>
+              <td class="col-title" align="center">予約情報の編集</td>
+            </tr>
+            @foreach ( $list3s as $list3 )
+            <tr>
+              <td>{{ $list3->clinic_name }}</td>
+              <td>{{ formatDate($list3->result_date, '/') }}</td>
+              <td>{{ formatDate($list3->booking_date, '/') }} {{splitHourMin($list3->booking_start_time)}}～{{toTime($list3->booking_start_time, $list3->booking_total_time)}}</td>
+              <td>{{ $list3->p_no }}</td>
+              <td>{{ $list3->p_name }}</td>
+              <td>{{ $list3->p_tel }}</td>
+              <td>
+                <!-- service 1 -->
+                @if ( $list3->service_1_kind == 1 )
+                  {{ @$services[$list3->service_1] }}
+                @elseif ( $list3->service_1_kind == 2 )
+                  {{ @$treatment1s[$list3->service_1] }}
+                @endif
+                <!-- service 2 -->
+                @if ( $list3->service_2_kind == 1 )
+                  {{ @$services[$list3->service_2] }}
+                @elseif ( $list3->service_2_kind == 2 )
+                  {{ @$treatment1s[$list3->service_2] }}
+                @endif
+              </td>
+              <td>{{ $list3->result_memo }}</td>
+              <td align="center"><input onclick="location.href='{{ route('ortho.bookings.booking.edit', [ $list3->booking_id ]) }}'" value="予約情報の編集" type="button" class="btn btn-xs btn-page"/></td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+        @endif
+      </div>
+    </div>    
+  </section>
+  <!-- End content list1 list -->
+@endsection

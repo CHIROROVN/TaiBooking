507b0ade8442b621565cdd9277d7c730ded1c2fc
dophu17 <dophu17@gmail.com>
2016-10-11 18:04:46 +0700

    update

---
+++ resources/views/backend/ortho/list1_list/edit.blade.php
@@ -0,0 +1,133 @@
+@extends('backend.ortho.ortho')
+
+@section('content')
+    {!! Form::open(array('route' => ['ortho.list1_list.edit', $list1->id], 'method' => 'post')) !!}
+      <section id="page">
+        <div class="container">
+          <div class="row content-page">
+            <h3>各種リスト表示　＞　「TEL待ちリスト」の表示</h3>
+              <div class="table-responsive">
+                <table class="table table-bordered">
+
+                  <!-- clinic_id -->
+                  <tr>
+                    <td class="col-title"><label for="telephone">医院名 <span class="note_required">※</span></label></td>
+                    <td>
+                      <select name="clinic_id" id="clinic_id" class="form-control form-control--medium">
+                        <option value="">▼選択</option>
+                        @foreach($clinics as $clinic)
+                          @if ( $list1->clinic_id == $clinic->clinic_id )
+                          <option value="{{$clinic->clinic_id}}" selected >{{$clinic->clinic_name}}</option>
+                          @else
+                          <option value="{{$clinic->clinic_id}}" >{{$clinic->clinic_name}}</option>
+                          @endif
+                        @endforeach
+                      </select>
+                    </td>
+                  </tr>
+
+                  <!-- patient_id -->
+                  <tr>
+                    <td class="col-title"><label for="telephone">患者名 <span class="note_required">※</span></label></td>
+                    <td>
+                      <input type="hidden" name="p_id" id="p_id" value="{{ $list1->patient_id }}">
+                      @foreach ( $patients as $patient )
+                        @if ( $patient->p_id == $list1->patient_id )
+                        <input type="text" name="patient" id="patient" class="input-text-mid form-control" style="width: 250px; display: inline;" value="{{ $patient->p_no }} {{ $patient->p_name_f }} {{ $patient->p_name_g }} ({{ $patient->p_name_f_kana }} {{ $patient->p_name_g_kana }})">
+                        @endif
+                      @endforeach
+                    </td>
+                  </tr>
+
+                  <!-- telephone -->
+                  <tr>
+                    <td class="col-title"><label for="telephone">電話番号 <span class="note_required">※</span></label></td>
+                    <td>
+                      <input type="text" name="telephone" id="telephone " class="form-control" value="{{ $list1->telephone }}" />
+                      <span class="error-input">@if ($errors->first('telephone')) ※{!! $errors->first('telephone') !!} @endif</span>
+                    </td>
+                  </tr>
+
+                  <!-- free_text -->
+                  <tr>
+                    <td class="col-title"><label for="free_text">備考</label></td>
+                    <td>
+                      <input type="text" name="free_text" id="free_text" class="form-control" value="{{ $list1->free_text }}" />
+                      <span class="error-input">@if ($errors->first('free_text')) ※{!! $errors->first('free_text') !!} @endif</span>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+          </div>
+          <div class="row margin-bottom">
+            <div class="col-md-12 text-center">
+              <input type="submit" name="save" value="登録する" class="btn btn-sm btn-page mar-right">
+              <button type="button" class="btn btn-sm btn-page" data-toggle="modal" data-target="#myModal">削除する</button>
+              <!-- Modal -->
+              <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog modal-sm">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                      <h4 class="modal-title">{{ trans('common.modal_header_delete') }}</h4>
+                    </div>
+                    <div class="modal-body">
+                      <p>{{ trans('common.modal_content_delete') }}</p>
+                    </div>
+                    <div class="modal-footer">
+                      <a href="{{ route('ortho.list1_list.delete', [$list1->id]) }}" class="btn btn-sm btn-page">{{ trans('common.modal_btn_delete') }}</a>
+                      <button type="button" class="btn btn-sm btn-page" data-dismiss="modal">{{ trans('common.modal_btn_cancel') }}</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- end modal -->
+            </div>
+          </div>
+          <div class="row margin-bottom">
+            <div class="col-md-12 text-center">
+              <a href="{{ route('ortho.list1_list.index') }}" class="btn btn-sm btn-page mar-right">登録済み地域一覧に戻る</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    {!! Form::close() !!}
+@stop
+
+@section('script')
+  <script>
+    $(document).ready(function(){
+      $( "#patient" ).autocomplete({
+        minLength: 0,
+        // source: pamphlets,
+        source: function(request, response){
+            var key = $('#patient').val();
+            $.ajax({
+                url: "{{ route('ortho.patients.autocomplete.patient') }}",
+                beforeSend: function(){
+                },
+                method: "GET",
+                data: { key: key },
+                dataType: "json",
+                success: function(data) {
+                  response(data);
+                },
+            });
+        },
+        focus: function( event, ui ) {
+          $( "#patient" ).val( ui.item.label );
+          return true;
+        },
+        select: function( event, ui ) {
+          $( "#patient" ).val( ui.item.label );
+          $( "#p_id" ).val( ui.item.value );
+          return false;
+        }
+      }).autocomplete( "instance" )._renderItem = function( ul, item ) {
+          return $( "<li>" )
+            .append( "<a>" + item.desc + "</a>" )
+            .appendTo( ul );
+      };
+    });
+  </script>
+@stop
---
+++ resources/views/backend/ortho/list1_list/index.blade.php
@@ -0,0 +1,157 @@
+@extends('backend.ortho.ortho')
+
+@section('content')
+<section id="page">
+  <div class="container content-page">
+    <h3>各種リスト表示　＞　「TEL待ちリスト」の表示</h3>
+
+    <div class="msg-alert-action margin-top-15">
+      @if ($message = Session::get('success'))
+        <div class="alert alert-success  alert-dismissible fade in" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <ul class="no-margin-bottom"><strong><li> {{ $message }}</li></strong></ul>
+        </div>
+      @elseif($message = Session::get('danger'))
+        <div class="alert alert-danger alert-dismissible fade in" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <ul class="no-margin-bottom"><strong><li> {{ $message }}</li></strong></ul>
+        </div>
+      @endif
+    </div>
+
+    <div class="row">
+      <div class="col-md-12" style="float: left; width: auto;">
+        {!! Form::open(array('route' => 'ortho.list1_list.index', 'method' => 'get')) !!}
+        <!-- p_id -->
+        <input type="hidden" name="p_id" id="p_id" value="{{ $p_id }}">
+        <input type="text" name="patient" id="patient" class="input-text-mid form-control" style="width: 250px; display: inline; float: left;" value="{{ $patient }}">
+        <!-- date -->
+        <input type="calendar" name="insert_date" id="insert_date" class="input-text-mid form-control datepicker" style="float: left; margin-left: 10px;" value="{{ $insert_date }}">
+        <!-- submit -->
+        <input type="submit" class="btn btn-sm btn-page" value="サーチ">
+        </form>
+      </div>
+      <div class="col-md-12 text-right" style="float: right; width: auto;">
+        <a href="{{ route('ortho.list1_list.regist') }}" class="btn btn-sm btn-page">TEL待ちリストの新規登録</a>
+      </div>
+    </div>
+    
+    <table class="table table-bordered treatment2-list">
+      <tbody>
+        <tr>
+          <td align="center" class="col-title">医院名</td>
+          <td align="center" class="col-title">登録日</td>
+          <td align="center" class="col-title">当初予約日時</td>
+          <td align="center" class="col-title">カルテNo</td>
+          <td align="center" class="col-title">患者名</td>
+          <td align="center" class="col-title">電話番号</td>
+          <td align="center" class="col-title">備考</td>
+          <td align="center" class="col-title col-edit">予約の登録</td>
+          <td align="center" class="col-title col-action">TEL待ち情報の編集</td>
+        </tr>
+
+        @if(empty($list1_list) || count($list1_list) < 1)
+          <tr>
+            <td colspan="8">
+              <h3 align="center">該当するデータがありません。</h3>
+            </td>
+          </tr>
+        @else
+          @foreach($list1_list as $item)
+          <tr>
+            <td>
+              @foreach ( $clinics as $clinic )
+                @if ( $clinic->clinic_id == $item->clinic_id )
+                  {{ $clinic->clinic_name }}
+                @endif
+              @endforeach
+            </td>
+            <td>{{ $item->insert_date }}</td>
+            <td>{{ $item->booking_date }}</td>
+            <td>
+              @foreach ( $patients as $patient )
+                @if ( $patient->p_id == $item->patient_id )
+                  {{ $patient->p_no }}
+                @endif
+              @endforeach
+            </td>
+            <td>
+              @foreach ( $patients as $patient )
+                @if ( $patient->p_id == $item->patient_id )
+                  {{ $patient->p_name_f }} {{ $patient->p_name_g }} ({{ $patient->p_name_f_kana }} {{ $patient->p_name_g_kana }})
+                @endif
+              @endforeach
+            </td>
+            <td>
+              {{ @$item->telephone }}
+            </td>
+            <td>
+              {{ @$item->free_text }}
+            </td>
+            <td><a href="{{ route('ortho.bookings.booking_search') }}" class="btn btn-sm btn-page">予約の登録</a></td>
+            <td><a href="{{ route('ortho.list1_list.edit', [ $item->id ]) }}" class="btn btn-sm btn-page">TEL待ち情報の編集</a></td>
+          </tr>
+          @endforeach
+        @endif
+
+      </tbody>
+    </table>
+  </div>
+</section>
+@stop
+
+@section('script')
+  <script>
+    $(document).ready(function(){
+      $( "#patient" ).autocomplete({
+        minLength: 0,
+        // source: pamphlets,
+        source: function(request, response){
+            var key = $('#patient').val();
+            $.ajax({
+                url: "{{ route('ortho.patients.autocomplete.patient') }}",
+                beforeSend: function(){
+                },
+                method: "GET",
+                data: { key: key },
+                dataType: "json",
+                success: function(data) {
+                  response(data);
+                },
+            });
+        },
+        focus: function( event, ui ) {
+          $( "#patient" ).val( ui.item.label );
+          return true;
+        },
+        select: function( event, ui ) {
+          $( "#patient" ).val( ui.item.label );
+          $( "#p_id" ).val( ui.item.value );
+          return false;
+        }
+      }).autocomplete( "instance" )._renderItem = function( ul, item ) {
+          return $( "<li>" )
+            .append( "<a>" + item.desc + "</a>" )
+            .appendTo( ul );
+      };
+
+
+      // datepicker
+      $.datepicker.setDefaults( $.datepicker.regional[ "ja" ] );
+      $(".datepicker").datepicker({
+          showOn: 'both',
+          buttonText: 'カレンダー',
+          buttonImageOnly: true,
+          buttonImage: "{{asset('public/backend/ortho/common/image/dummy-calendar.png')}}",
+          dateFormat: 'yy-mm-dd',
+          constrainInput: true,
+          inline: true,
+          lang: 'ja'
+      });
+    });
+  </script>
+@stop
